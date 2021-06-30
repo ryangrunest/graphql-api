1fee9bcc957689521941171003bd048fac3be4ed
Ryan Grunest <ryangrunest@geeko>
2021-06-30 15:19:44 -0700

    user can filter list of sessions

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const SessionAPI = require("./datasources/sessions.js");
 
 const typeDefs = gql`
   type Query {
-    sessions: [Session]
+    sessions(
+      id: ID
+      title: String
+      description: String
+      startsAt: String
+      endsAt: String
+      room: String
+      day: String
+      format: String
+      track: String
+      level: String
+    ): [Session]
     sessionById(id: ID): Session
   }
   type Session {
